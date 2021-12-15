d3ca20efb5ab3cc73868d8c2adccba37ffd29dcd
Mohamed Bifenzi <mbifenzi@e2r2p3.1337.ma>
2021-12-15 19:24:20 +0100

    first ex

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test.cpp                                           :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbifenzi <mbifenzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 18:45:35 by mbifenzi          #+#    #+#             */
-/*   Updated: 2021/12/15 18:47:22 by mbifenzi         ###   ########.fr       */
+/*   Updated: 2021/12/15 19:23:38 by mbifenzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main(void)
+int main(int argc, char **argv)
 {
-    std::cout << "simo bifenzi" << std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else if(argc >= 2)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+			{
+				char s = toupper(argv[i][j]);
+				std::cout << s;
+			}
+			std::cout << " ";
+		}
+			std::cout << std::endl;
+		
+    }
     return (0);
 }
